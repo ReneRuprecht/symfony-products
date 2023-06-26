c669b8b8a4252e405553667946ba060cadb243c2
ReneRuprecht <rene.ruprecht.396@gmail.com>
2023-06-26 22:22:04 +0200

    Add: Database connection and product entity

---
+++ backend/src/Entity/Product.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?int $regular_price = null;
+
+    #[ORM\Column]
+    private ?int $discount_price = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $market_name = null;
+
+    #[ORM\Column]
+    private ?int $postal_code = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $created_at = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $updated_at = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getRegularPrice(): ?int
+    {
+        return $this->regular_price;
+    }
+
+    public function setRegularPrice(int $regular_price): static
+    {
+        $this->regular_price = $regular_price;
+
+        return $this;
+    }
+
+    public function getDiscountPrice(): ?int
+    {
+        return $this->discount_price;
+    }
+
+    public function setDiscountPrice(int $discount_price): static
+    {
+        $this->discount_price = $discount_price;
+
+        return $this;
+    }
+
+    public function getMarketName(): ?string
+    {
+        return $this->market_name;
+    }
+
+    public function setMarketName(string $market_name): static
+    {
+        $this->market_name = $market_name;
+
+        return $this;
+    }
+
+    public function getPostalCode(): ?int
+    {
+        return $this->postal_code;
+    }
+
+    public function setPostalCode(int $postal_code): static
+    {
+        $this->postal_code = $postal_code;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $created_at): static
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updated_at;
+    }
+
+    public function setUpdatedAt(\DateTimeImmutable $updated_at): static
+    {
+        $this->updated_at = $updated_at;
+
+        return $this;
+    }
+}
